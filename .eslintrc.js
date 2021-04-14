2b70f1ba8708fb6e8a211344b1fe6a0f0c2c46dc
Siddharth Jain <siddharthjain125@gmail.com>
2021-04-15 01:28:38 +0530

    First Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,24 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-    ecmaFeatures: {
-      jsx: true,
+    "env": {
+        "browser": true,
+        "es2021": true
     },
-    sourceType: 'module',
-  },
-  plugins: ['react', 'react-hooks'],
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  settings: {
-    react: {
-      version: '16.x',
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
     },
-  },
-  rules: {
-    'react/prop-types': ['off'],
-    'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
-  },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "react/prop-types": "off"
+    }
 };
